b95c59802bc11592656c9d98562d6ae8a9b86066
Thiago Araújo <thiagoaraujoadvg@gmail.com>
2025-09-29 15:10:46 -0300

    Refactor configuration settings for improved clarity and maintainability

--- app/core/config.py
+++ app/core/config.py
@@ -28,7 +28,8 @@ class Settings(BaseSettings):
     
     # Lista de origens permitidas para CORS (Cross-Origin Resource Sharing)
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost:3000",
+        "http://localhost:3000",  # URL do frontend React
+        "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://localhost:8080",
         "http://localhost",
